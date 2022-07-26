1f9217fc9b56a3add77615d120521a2afa5b2bdf
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-07-26 07:30:13 -0500

    Update PREP Template. Add Number-of-Steps-to-Reduce-a-Number-to-Zero.js

--- PREP-Template.js
+++ PREP-Template.js
@@ -1,23 +1,24 @@
 //-----------MY SOLUTION--------------//
 
-//PREP
-//Parameters: 
-// What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
-// Will there ever be an invalid input? Do I need to account for unexpected edge cases?
+/*
+//----------PREP---------//
+//----------Parameters---------//
+What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
+Will there ever be an invalid input? Do I need to account for unexpected edge cases?
 
-//Return: 
-// What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
-// Will the expected return ever change for invalid inputs or edge cases?
+//----------Return---------//
+What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
+Will the expected return ever change for invalid inputs or edge cases?
 
-//Examples: 
-// Check your understanding! Are tests provided that you can reference as examples? What would happen in invalid or edge situations? 
-// Aim for at least 3 examples.
+//----------Examples---------//
+Check your understanding! Are tests provided that you can reference as examples? What would happen in invalid or edge situations? 
+Aim for at least 3 examples.
   
-//Psuedocode:
-// In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
-// Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
-// Good pseudocode only comes with practice.
-   
+//----------Psuedocode---------//
+In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
+Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
+Good pseudocode only comes with practice.
+*/
   
 //----------BEST PRACTICES SOLUTION----------//
 
