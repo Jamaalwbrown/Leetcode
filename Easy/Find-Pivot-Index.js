6974453913580f51604f36c8709f5b3628a47934
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-08-01 00:26:57 -0500

    Update Find-Pivot-Index.js

--- Easy/Find-Pivot-Index.js
+++ Easy/Find-Pivot-Index.js
@@ -48,5 +48,57 @@ Left sum = 0 (no elements to the left of index 0)
 Right sum = nums[1] + nums[2] = 1 + -1 = 0
   
 //----------Psuedocode---------//
--Take the array and 
+-Check if array length of num is 1. If so return 0.
+-Set up an iteration for the num array
+-On each iteration sum the elements to the left and to the right of i. 
+-Compare the left sum with the right sum and return true or false
+    - The above two steps will be done with a helper function called checkforPivot
+-if the two sums match then we return i
+-if the two sums do not match then we continue the iteration
+-if we reach the end of the array with the checkforPivot function not returning true then we return -1
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+ var pivotIndex = function(nums) {
+    if(nums.length === 1) {return 0}
+    let result = -1;
+    
+    for (let i=0; i < nums.length; i++) {
+        if(checkForPivot(i, nums)) {
+            result = i;
+            break;
+        }
+    }
+    
+    return result;
+};
+
+
+
+let checkForPivot = function(index, array) {
+    let leftSum = 0, rightSum = 0;
+    for(let j=0; j < index; j++) {
+        leftSum += array[j]
+    }
+    
+    for(let k=(array.length - 1); k > index; k-- ) {
+        rightSum += array[k]
+    }
+    
+    if (rightSum === leftSum) {
+        return true
+    }
+    
+    else {
+        return false
+    }
+}
+
+console.log(pivotIndex([1,7,3,6,5,6]));
+console.log(pivotIndex([1,2,3]));
+console.log(pivotIndex([2,1,-1]));
+
+
