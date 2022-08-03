20bb8e6a96a2849ec53d9e248a97131dc4f989eb
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-08-02 23:53:44 -0500

    Add Isomorphic-Strings.js

---
+++ Easy/Isomorphic-Strings.js
@@ -0,0 +1,29 @@
+/*
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+*/
+
+//-----------MY SOLUTION--------------//
+
+/*
+//----------PREP---------//
+//----------Parameters---------//
+What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
+Will there ever be an invalid input? Do I need to account for unexpected edge cases?
+
+//----------Return---------//
+What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
+Will the expected return ever change for invalid inputs or edge cases?
+
+//----------Examples---------//
+Check your understanding! Are tests provided that you can reference as examples? What would happen in invalid or edge situations? 
+Aim for at least 3 examples.
+  
+//----------Psuedocode---------//
+In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
+Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
+Good pseudocode only comes with practice.
+*/
