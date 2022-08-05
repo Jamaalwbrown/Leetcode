9edefe7f1de011354b3999f07d0a98f35aefcf7a
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-08-04 23:52:53 -0500

    Add Two-Sum.js

---
+++ Easy/Two-Sum.js
@@ -0,0 +1,44 @@
+/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+You can return the answer in any order.
+*/
+
+//-----------MY SOLUTION--------------//
+
+/*
+//----------PREP---------//
+//----------Parameters---------//
+What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
+Will there ever be an invalid input? Do I need to account for unexpected edge cases?
+
+//----------Return---------//
+What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
+Will the expected return ever change for invalid inputs or edge cases?
+
+//----------Examples---------//
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+
+
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+
+
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+  
+//----------Psuedocode---------//
+In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
+Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
+Good pseudocode only comes with practice.
+*/
