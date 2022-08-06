9dd55b2c7ea6f3e122fe4f06531719ef7d03db3b
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-08-05 23:57:59 -0500

    Add Valid-Parentheses.js

---
+++ Easy/Valid-Parentheses.js
@@ -0,0 +1,31 @@
+/*
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order
+*/
+
+
+//-----------MY SOLUTION--------------//
+
+/*
+//----------PREP---------//
+//----------Parameters---------//
+What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
+Will there ever be an invalid input? Do I need to account for unexpected edge cases?
+
+//----------Return---------//
+What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
+Will the expected return ever change for invalid inputs or edge cases?
+
+//----------Examples---------//
+Check your understanding! Are tests provided that you can reference as examples? What would happen in invalid or edge situations? 
+Aim for at least 3 examples.
+  
+//----------Psuedocode---------//
+In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
+Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
+Good pseudocode only comes with practice.
+*/
