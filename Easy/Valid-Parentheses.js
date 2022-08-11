edcdaa17b9830070abe31d8bf4de7c6716852ff7
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-08-10 23:37:53 -0500

    Update Valid-Parentheses.js

--- Easy/Valid-Parentheses.js
+++ Easy/Valid-Parentheses.js
@@ -13,12 +13,16 @@ Open brackets must be closed in the correct order
 /*
 //----------PREP---------//
 //----------Parameters---------//
-What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
-Will there ever be an invalid input? Do I need to account for unexpected edge cases?
+-We are given a string s. s will have just brackets (, ), {, }, [, and ]
+-1 <= s.length <= 104
+-s consists of parentheses only '()[]{}'.
 
 //----------Return---------//
-What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
-Will the expected return ever change for invalid inputs or edge cases?
+-We return a boolean: true or false.
+-We return true if the input string s is valid, meaning:
+    -Open brackets must be closed by the same type of brackets
+    -Open brackets must be closed in the correct order.
+-We will return false otherwise
 
 //----------Examples---------//
 Check your understanding! Are tests provided that you can reference as examples? What would happen in invalid or edge situations? 
@@ -42,7 +46,56 @@ Input: s = "(]"
 Output: false
   
 //----------Psuedocode---------//
-In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
-Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
-Good pseudocode only comes with practice.
-*/
+-First we check if the length of s is odd. If so we already know we have an invalid string because we must have an even number of charcters for there to be a matching opening and closing bracket
+-We will need to iterate through the string character by character
+-For each character that is an opening bracket we take its value and store it on a stack we declare
+-When we get to a closing bracket, we pop the previous value on the stack and check if it matches.
+    --if there is no match then we have an invalid string and we return false
+    --if there is a matching opening tag then we keep going
+-popping the value before comparing will essentially allow us to work backwards through any opening tags we already encountered while moving forward through the string to compare more end brackets
+
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s) {
+    if(s.length % 2 !== 0) {
+        return false
+    }
+
+    let tempStack = [];
+    for (let i = 0; i < s.length; i++) {
+        console.log(tempStack);
+        if(s[i] === '(' || s[i] === '[' || s[i] === '{') {
+            tempStack.push(s[i]);
+        }
+
+        if(s[i] === ')') {
+            if(tempStack.pop() === '[' || tempStack.pop() === '{') {
+                return false
+            }
+        }
+
+        if(s[i] === ']') {
+            if(tempStack.pop() === '(' || tempStack.pop() === '{') {
+                console.log('Found error')
+                return false;
+            }
+        }
+
+        if(s[i] === '}') {
+            if(tempStack.pop() === '[' || tempStack.pop() === '(') {
+                return false
+            }
+        } 
+    }
+    return true
+};
+
+isValid("()");
+isValid("()[]{}");
+isValid("(]");
+isValid("{[]}");
+
+
