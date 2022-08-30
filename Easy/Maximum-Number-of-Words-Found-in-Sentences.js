25610d94be882adc593dbb2a09c7ecb0d9298fa1
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-08-30 00:48:14 -0500

    Update Maximum Number of Words Found in Sentences

--- Easy/Maximum-Number-of-Words-Found-in-Sentences.js
+++ Easy/Maximum-Number-of-Words-Found-in-Sentences.js
@@ -48,10 +48,6 @@ Aim for at least 3 examples.
   
 //----------Psuedocode---------//
 Look at each sentence in the array and count how many words are in them. If the total amount of words in a sentence exceeds the max we already have replace it.
-
-In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
-Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
-Good pseudocode only comes with practice.
 */
 
 /**
@@ -66,13 +62,11 @@ Good pseudocode only comes with practice.
     let max = 0, sentenceLength = 0;
     for(let i = 0; i < sentences.length; i++) {
         sentenceLength = sentences[i].split(' ').length
-        console.log(sentenceLength)
         if (sentenceLength > max) {
             max = sentenceLength;
         } 
     }
 
-    console.log(max);
+    return max;
 };
 
-mostWordsFound(["please wait", "continue to fight", "continue to win"]);
