0afbf1151b46f2d9ac8ccc2fc0847be3e097319b
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-09-12 18:30:58 -0500

    Update How-Many-Numbers-Are-Smaller-Than-the-Current-Number

--- Easy/How-Many-Numbers-Are-Smaller-Than-the-Current-Number.js
+++ Easy/How-Many-Numbers-Are-Smaller-Than-the-Current-Number.js
@@ -3,10 +3,18 @@
 /*
 //----------PREP---------//
 //----------Parameters---------//
+-We are given an array of numbers called nums
+-The numbers in the nums array nums[i] range from 0 <= nums[i] <= 100
+-The length of the nums array is 0 <= nums[i] <= 100
+
+
 What are they giving me? What are the expected arguments? Will it only ever be one type of data, or do I need to account for others? 
 Will there ever be an invalid input? Do I need to account for unexpected edge cases?
 
 //----------Return---------//
+-We return an array of counts. Each count in the array represents the count of numbers that are smaller than the number in the same position as the count in the passed in array
+- 
+
 What do they want me to return? Are they expecting a string? array? object? Is there a certain format the return is expected in? 
 Will the expected return ever change for invalid inputs or edge cases?
 
@@ -35,7 +43,41 @@ Input: nums = [7,7,7,7]
 Output: [0,0,0,0]
 
 //----------Psuedocode---------//
-In fairly plain speech, how would you solve this? What needs to happen to get from the starting inputs to the desired return? 
-Think it through step by step, if you write something down and it is more than a single action, you may need to break it down more. 
-Good pseudocode only comes with practice.
+Check if the array nums is empty. If so return an empty array (assumption).
+Iterate through the array
+    For each number we check if all the other numbers in the array are less than the element we are on
+    For each element that is less than the element we are on, increment a count variable
+    Once we are done checking all the other numbers, we place the count variable on a result array in the same position that we are in, in the passed in array
+    If there are no numbers less than our current number then we store 0 in the array
+
 */
+
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+ var smallerNumbersThanCurrent = function(nums) {
+    if (nums.length === 0) {
+        return []
+    }
+
+    const resultArr = [];
+    for (let i = 0; i < nums.length; i++) {
+
+        let count = 0;
+        for (let j = 0; j < nums.length; j++) {
+            if (j !== i) {
+                if (nums[j] < nums[i]) {
+                    count++;
+                } 
+            }
+        }
+        resultArr.push(count);
+    }
+
+    return resultArr;
+};
+
+console.log(smallerNumbersThanCurrent([8,1,2,2,3]));
+console.log(smallerNumbersThanCurrent([6,5,4,8]));
+console.log(smallerNumbersThanCurrent([7,7,7,7]));
