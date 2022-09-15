98c530b769f5072d8aaaa092c6f8236162942425
Cautious Engineer <jamaalwbrown@utexas.edu>
2022-09-14 23:59:23 -0500

    Updating comments

--- Easy/How-Many-Numbers-Are-Smaller-Than-the-Current-Number.js
+++ Easy/How-Many-Numbers-Are-Smaller-Than-the-Current-Number.js
@@ -69,7 +69,8 @@ Iterate through the array
 
     return resultArr;
 };
-
+/*Testing 
 console.log(smallerNumbersThanCurrent([8,1,2,2,3]));
 console.log(smallerNumbersThanCurrent([6,5,4,8]));
 console.log(smallerNumbersThanCurrent([7,7,7,7]));
+*/
